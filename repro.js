ca37d09cbc62a16ba5d318ec5bdbfde26f76b971
Henric Trotzig <henric.trotzig@gmail.com>
2021-02-19 12:47:47 +0100

    Attempt to repro airbnb issue
    I'm getting reports that some elements with images have spurious diffs,
    possibly related to rounded borders. This is my attempt at reproducing
    the issue (I couldn't).

--- repro.js
+++ repro.js
@@ -3,25 +3,12 @@ const crypto = require('crypto');
 const fs = require('fs');
 
 function createHash(data) {
-  return crypto
-    .createHash('md5')
-    .update(data)
-    .digest('hex');
+  return crypto.createHash('md5').update(data).digest('hex');
 }
 
 async function takeScreenshot(page) {
-  await page.setContent(`
-    <style>
-      main {
-        border-radius: 3px;
-        height: 20px;
-        width: 20px;
-        border:1px solid rgba(20,171,142,0.3);
-      }
-    </style>
-    <main />
-  `);
-  const main = await page.$('main');
+  await page.setContent(fs.readFileSync('./airbnb.html', 'utf-8'));
+  const main = await page.$('div');
   const pngBuf = await main.screenshot();
   const hash = createHash(pngBuf);
   fs.writeFileSync(`${hash}.png`, pngBuf);
@@ -44,7 +31,9 @@ async function run() {
 }
 
 run().then(() => {
-  console.log('Press a key to exit (or leave open while you copy screenshots to your local machine using `docker cp <container_id>:/usr/src/app/<file_name.png> .`)');
+  console.log(
+    'Press a key to exit (or leave open while you copy screenshots to your local machine using `docker cp <container_id>:/usr/src/app/<file_name.png> .`)',
+  );
   process.stdin.setRawMode(true);
   process.stdin.resume();
   process.stdin.on('data', process.exit.bind(process, 0));
